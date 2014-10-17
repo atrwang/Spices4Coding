3aa3e9560658b9907278d60ae300291936da83f5
kevin.wang <atrwang@gmail.com>
2014-10-17 21:50:26 +0800

    Update

--- ftpupload.sh
+++ ftpupload.sh
@@ -2,7 +2,7 @@
 SERVER="devapp.plaso.cn"
 USER="plasoftpuser"
 PASS="hongfeizhifei9989"
-LOCALDIR="/Library/Server/Web/Data/Sites/Default/tools"
+LOCALDIR="/Library/Server/Web/Data/Sites/Default/~kevin"
 REMOTEDIR="plaso_website/prod/"
 
 ftp -v -n $SERVER <<END_OF_SESSION
@@ -10,7 +10,9 @@ user $USER $PASS
 $FILETYPE
 lcd $LOCALDIR
 cd $REMOTEDIR
-mput *
-mput *.*
+put Dev_Plaso4StudentClientBeta.ipa kevin_DevPlaso4StudentClientBeta.ipa
+put Dev_Plaso4TeacherClientBeta.ipa kevin_DevPlaso4TeacherClientBeta.ipa
+put Plaso4StudentClientBeta.ipa kevin_Plaso4StudentClientBeta.ipa
+put Plaso4TeacherClientBeta.ipa kevin_Plaso4TeacherClientBeta.ipa
 bye     
 END_OF_SESSION
